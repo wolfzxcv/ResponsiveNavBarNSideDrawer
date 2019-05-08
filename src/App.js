06ba01ead6533d78852374deb22e1671f5a18e38
Nien <wolfzxcv@gmail.com>
2019-05-08 07:19:44 +0100

    pre passing props

--- src/App.js
+++ src/App.js
@@ -7,17 +7,12 @@ import Backdrop from './Components/Backdrop'
 import GlobalStyle from './theme/GlobalStyle'
 
 
-const handleDrawerOpen = props => {
-  const [ toggle , setToggle] = React.useState(false);
-  return { props => setToggle( !toggle)}
-}  
-
 const App = ({className}) => (
         
      <div className={className}>
      <GlobalStyle />
      <Toolbar />
-     { toggle ? <SideDrawer /><Backdrop />  : ''}       
+       
      <main className='content'><p>This is the page content!</p></main>
     </div>
   );
